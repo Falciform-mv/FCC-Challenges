d2eb2e58b3d37403f1950227eb649003fcc7405e
Mike Veneziale <falciformdesign@gmail.com>
2021-08-15 20:31:08 -0500

    updated folder

---
+++ myApp.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
+
+
+ console.log("Hello World");
